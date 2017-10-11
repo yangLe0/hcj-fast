ec58c9d7b5d35cdd7ec4928f4433414eb2e59252
yangLe0 <32234018+yangLe0@users.noreply.github.com>
2017-10-11 16:37:12 +0800

    layout1

--- img.js
+++ img.js
@@ -11,8 +11,35 @@ function imgLocation(parent,content){
 	var ccontent = getChildElement(cparent,content);
 	//console.log(ccontent.length);
 	var imgWidth = ccontent[0].offsetWidth;
-	var cols = Math.floor(document.documentElement.clientWidth / imgWidth);
-	cparent.style.cssText = "width:"+imgWidth * cols + "px;margin:0 auto";//固定图片显示个数
+	var num = Math.floor(document.documentElement.clientWidth / imgWidth);
+	cparent.style.cssText = "width:"+imgWidth * num + "px;margin:0 auto";//固定图片显示个数
+	
+	//下一行第一个图片接上行最短的图片下面，所以先找图片高度
+	var BoxHeightArr = [];
+	for(var i = 0;i<ccontent.length;i++){
+
+		if(i<num){
+			BoxHeightArr[i] = ccontent[i].offsetHeight;
+			console.log(BoxHeightArr[i]);//打印第一行图片高度
+		}else{
+			var minHeight = Math.min.apply(null,BoxHeightArr);
+			var minIndex = getminheightLocation(BoxHeightArr,minHeight);
+			
+			console.log(minHeight);//最小图片高度
+			ccontent[i].style.position = "absolute";
+			ccontent[i].style.top = minHeight + "px";
+			ccontent[i].style.left = ccontent[minIndex].offsetLeft + "px";
+			BoxHeightArr[minIndex] = BoxHeightArr[minIndex] + ccontent[i].offsetHeight;
+		}
+	}
+}
+//得到下一行图片摆放位置
+function getminheightLocation(BoxHeightArr,minHeight){
+	for(var i in BoxHeightArr){
+		if(BoxHeightArr[i] == minHeight){
+			return i;
+		}
+	}
 }
 
 function getChildElement(parent,content){

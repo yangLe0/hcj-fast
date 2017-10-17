90765f13ed76002d7eafb2e7249226c068307b6e
yangLe0 <32234018+yangLe0@users.noreply.github.com>
2017-10-17 21:27:12 +0800

    jQuery扩展

---
+++ myjQuery.js
@@ -0,0 +1,14 @@
+/* $.myjq = function(){
+	alert("Hello myjQuery");
+}
+
+$.fn.myjq = function(){
+	$(this).text("Hello");
+} */
+
+//noConflict
+$(document).ready(function(){
+	$("btn").on("click",function(){
+		
+	});
+});
